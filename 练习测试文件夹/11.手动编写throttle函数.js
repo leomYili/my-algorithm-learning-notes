10cade8043e94239e1a648a3cc1eb962dc34a9db
leomYili <leomy@outlook.com>
2021-08-17 19:29:33 +0800

    feat: 8.17题目汇总

---
+++ 练习测试文件夹/11.手动编写throttle函数.js
@@ -0,0 +1,19 @@
+/**
+ * 节流函数共有两个参数,一个是延迟执行的回调,另一个是等待时间
+ *
+ * @param {*} fn
+ * @param {*} wait
+ */
+function throttle(fn, wait = 50) {
+  let self = this;
+  let prev = 0;
+
+  return (...args) => {
+    let now = +new Date();
+
+    if (now - prev > wait) {
+      prev = now;// 这里需要记忆,否则下次就没办法继续节流了
+      fn.apply(self, args);
+    }
+  };
+}
---
+++ 练习测试文件夹/12.手动编写debounce.js
@@ -0,0 +1,22 @@
+/**
+ * 防抖函数主要用于解决一段时间之内的无限触发阻止,直到停止触发之后的大于wait时间,才会真正执行
+ * 当options中有关于是否第一次触发时,可以加上相应判断
+ * @param {*} fn
+ * @param {*} wait
+ */
+function debounce(fn, wait = 0, immediate) {
+  let timer = null;
+  let self = this;
+
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+
+    if (immediate && !timer) {
+      fn.apply(self, args);
+    }
+
+    timer = setTimeout(() => {
+      fn.apply(self, args);
+    }, wait);
+  };
+}
